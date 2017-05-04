a3755426d879726865154dc1f3b2d11033025a8f
Marvin Roger <nivramdu94@gmail.com>
2017-05-04 11:37:24 +0200

    :art: Few stylistic changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,53 @@
   <v-container fluid="fluid">
     <v-row>
       <v-col xs9 class="main_view">
-        <h1>Cargo</h1>
+        <h1><img src="../assets/img/cargo_text.png" alt="Cargo"></h1>
 
         <h2>Sécurisez votre utilisation du téléphone en voiture.</h2>
+
+        <v-row>
+          <v-col xs12 md4>
+            <v-card>
+              <v-card-text>
+                <div>
+                  <b>Des contrôles accessibles</b><br>
+
+                  De gros boutons pour vous permettre d'effectuer des actions courantes
+                  sans lever les yeux de la route.
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col xs12 md4>
+            <v-card>
+              <v-card-text>
+                <div>
+                  <b>Un contrôle vocal</b><br>
+
+                  Écoutez et répondez à vos messages SMS, Messenger et Whatsapp avec
+                  le contrôle vocal.
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col xs12 md4>
+            <v-card>
+              <v-card-text>
+                <div>
+                  <b>Partage de position</b><br>
+
+                  Partagez votre position avec vos proches lors de longs trajets.
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row class="badges">
+          <v-col xs12>
+            <img src="../assets/img/google-play-badge.png" alt="Play Store">
+          </v-col>
+        </v-row>
       </v-col>
       <v-col xs3>
         <img src="../assets/img/screenshot.png" alt="Screenshot">
@@ -25,5 +69,13 @@ export default {
 
 .main_view h1, .main_view h2 {
   color: #fff;
+}
+
+.main_view h2 {
+  font-size: 30px;
+}
+
+.badges {
+  margin-top: 20px;
 }
 </style>
--- pages/track.vue
+++ pages/track.vue
@@ -4,7 +4,35 @@
       <v-col xs9 class="map_container">
         <div id="map" ref="map" />
       </v-col>
-      <v-col xs3>
+      <v-col xs3 class="map_data">
+        <v-card class="green darken-1 white--text">
+          <v-card-text>
+            <div>
+              Trajet terminé à <b>12h30</b>.
+            </div>
+          </v-card-text>
+        </v-card>
+
+        <v-card class="blue darken-1 white--text">
+          <v-card-text>
+            <div>
+              Trajet démarré à <b>12h26</b>.<br>
+              Dernière mise à jour : <b>12h30 (il y a 20min)</b>
+            </div>
+          </v-card-text>
+        </v-card>
+
+        <v-card>
+          <v-card-text>
+            <div>
+              <v-icon>flight_takeoff</v-icon> Départ à <b>12h30</b><br>
+              <v-icon>hourglass_empty</v-icon> <b>20 minutes</b> de trajet<br>
+              <v-icon>trending_up</v-icon> <b>50km</b> parcourus<br>
+              <v-icon>linear_scale</v-icon> <b>100km/h</b> de vitesse moyenne<br>
+              <v-icon>trending_down</v-icon> <b>50km</b> restants<br>
+            </div>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -21,7 +49,12 @@ export default {
   },
   data () {
     return {
-      id: null
+      id: null,
+      map: null,
+      fromMarker: null,
+      toMarker: null,
+      positionMarker: null,
+      pathPolyline: null
     }
   },
   created () {
@@ -42,16 +75,47 @@ export default {
   },
   methods: {
     initMap () {
-      var uluru = {lat: -25.363, lng: 131.044}
-      var map = new window.google.maps.Map(this.$refs.map, {
-        zoom: 4,
+      var uluru = {lat: -20.363, lng: 131.044}
+      var uluru2 = {lat: -25.363, lng: 131.044}
+      var uluru3 = {lat: -30.363, lng: 131.044}
+      this.map = new window.google.maps.Map(this.$refs.map, {
+        zoom: 5,
         center: uluru
       })
-      const markers = []
-      markers.push(new window.google.maps.Marker({
+      this.fromMarker = new window.google.maps.Marker({
+        icon: '/map-icons/marker_blue.png',
+        title: 'Position de départ',
         position: uluru,
-        map: map
-      }))
+        map: this.map
+      })
+      this.toMarker = new window.google.maps.Marker({
+        icon: '/map-icons/marker_red.png',
+        title: "Position d'arrivée",
+        position: uluru3,
+        map: this.map
+      })
+      this.positionMarker = new window.google.maps.Marker({
+        icon: '/map-icons/person.png',
+        title: 'Position actuelle',
+        position: uluru2,
+        map: this.map
+      })
+
+      const flightPlanCoordinates = [
+        {lat: 37.772, lng: -122.214},
+        {lat: 21.291, lng: -157.821},
+        {lat: -18.142, lng: 178.431},
+        {lat: -27.467, lng: 153.027}
+      ]
+
+      this.pathPolyline = new window.google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#e74c3c',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: this.map
+      })
     }
   }
 }
@@ -65,5 +129,13 @@ html, body, #__nuxt, .container, .row_full, .map_container {
 #map {
   width: 100%;
   height: 100%;
+}
+
+.map_container {
+  padding: 0 !important;
+}
+
+.map_data {
+  padding: 0 !important;
 }
 </style>
